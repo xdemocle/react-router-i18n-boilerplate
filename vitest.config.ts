4c4b7bea5c131a8600b07b2da21950500c15e04f
xdemocle <xdemocle@gmail.com>
2025-05-20 19:13:58 +0200

    chore: migrate test config from vitest to playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,5 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  test: {
-    reporters: [
-      ['junit', { outputFile: './junit.xml' }],
-    ],
-    browser: {
-      enabled: true,
-      provider: 'playwright',
-      instances: [
-        { browser: 'chromium' },
-      ],
-    },
-  },
+  reporter: [['junit', { outputFile: 'junit.xml' }]],
 });
